45bb44ccf756a7c0e9f8f081c66c2df96c9158d2
Victor Wonu <victorwonu@gmail.com>
2024-06-01 21:28:56 +0100

    corrected spell error

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -5,7 +5,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import javax.annotation.PostConstruct;
+import java.annotation.PostConstruct;
 
 @SpringBootApplication
 public class Application {
